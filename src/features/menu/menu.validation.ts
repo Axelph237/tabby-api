f10603692f16edbf83a4602ac2601bfddbc078af
CandyCaneCazoo <aiden.j.king004@gmail.com>
2025-05-23 18:39:19 -0700

    Made style field nullable

--- src/features/menu/menu.validation.ts
+++ src/features/menu/menu.validation.ts
@@ -6,11 +6,11 @@ export const menuTObj = t.Object({
 	created_at: t.Date(),
 	created_by: uuidTObj,
 	name: t.String(),
-	style: t.Optional(t.Object({
+	style: t.Nullable(t.Optional(t.Object({
 		colors: t.Optional(t.String()),
 		backgroundImgSrc: t.Optional(t.String({ format: "uri" })),
 		logoImgSrc: t.Optional(t.String({ format: "uri" })),
-	}))
+	})))
 })
 export type Menu = Static<typeof menuTObj>
 
