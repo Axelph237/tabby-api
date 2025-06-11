deff5dc7ee4f19f88cefe4e778a99d1b6b8a49c7
CandyCaneCazoo <58153713+Axelph237@users.noreply.github.com>
2025-06-10 23:42:32 -0700

    Added generic typing to NeonQueryResult

--- src/utils/types/drizzle/queries.ts
+++ src/utils/types/drizzle/queries.ts
@@ -3,7 +3,7 @@ import { SelectedFields } from 'drizzle-orm'
 import { PgRaw } from "drizzle-orm/pg-core/query-builders/raw";
 import { NeonHttpQueryResult } from "drizzle-orm/neon-http";
 
-export type NeonQueryResult = NeonHttpQueryResult<Record<string, unknown>>;
+export type NeonQueryResult<T = Record<string, unknown>> = NeonHttpQueryResult<T>;
 export type SelectProjection<TTable extends PgTableWithColumns<any>> = SelectedFields<any, TTable>;
 export type AnySimpleQuery = PgSelect<any> | PgInsert<any> | PgUpdate<any> | PgDelete<any> | PgRaw<any>
 export type AnyQuery = AnySimpleQuery | PgTransaction<any>
